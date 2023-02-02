6b41b67c4ca346fe72b3090fc9e872c9da8b83b2
duyngnth <duy.ngnth.work@gmail.com>
2023-02-02 14:17:31 +0700

    Test code in main

--- src/main/java/asia/ncc/application/App.java
+++ src/main/java/asia/ncc/application/App.java
@@ -1,13 +1,37 @@
 package asia.ncc.application;
 
+import asia.ncc.application.dto.EmployeeDTO;
+import asia.ncc.application.dto.ProjectDTO;
+import asia.ncc.application.entity.Employee;
+import asia.ncc.application.entity.Project;
+import asia.ncc.application.repository.EmployeeRepository;
+import asia.ncc.application.repository.ProjectAssignmentRepository;
+import asia.ncc.application.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Scanner;
+
 @SpringBootApplication
-public class App {
+@RequiredArgsConstructor
+public class App implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
+    private final ProjectAssignmentRepository projectAssignmentRepository;
+    private final EmployeeRepository employeeRepository;
+    private final ProjectRepository projectRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+    }
 }
--- src/main/java/asia/ncc/application/controller/DraftController.java
+++ src/main/java/asia/ncc/application/controller/DraftController.java
@@ -1,10 +1,14 @@
 package asia.ncc.application.controller;
 
+import asia.ncc.application.dto.EmployeeDTO;
+import asia.ncc.application.entity.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class FetchAPIController {
+public class DraftController {
     @RequestMapping("/fetchAPI")
     public String fetchAPI() {
         return "fetchAPI";
