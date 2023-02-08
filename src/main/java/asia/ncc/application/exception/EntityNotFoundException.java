6f4459dfb6289e3d04f06a7d76d2b3df04bfdde9
duyngnth <duy.ngnth.work@gmail.com>
2023-02-08 17:20:10 +0700

    Exception handling

---
+++ src/main/java/asia/ncc/application/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package asia.ncc.application.exception;
+
+public class EntityNotFoundException extends Exception {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
